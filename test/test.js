25d4eba8025b49100f5df4f548ee5ecb71eb57c6
Pan Rufeng <xingzhikong@126.com>
2018-11-02 11:31:39 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,17 +1,8 @@
 var assert = require('assert');
-var calc = require('../calc');
-var app = require('../app');
-
-describe('Calculator', function() {
-  describe('add', function() {
-    it('add(1,2) should return 3', function() {
-      assert.equal(calc.add(1,2),3);
-    });
-  });
-
-  describe('minus', function() {
-    it('minus(1,2) should return -1', function() {
-      assert.equal(calc.minus(1,2),-1);
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
     });
   });
 });
