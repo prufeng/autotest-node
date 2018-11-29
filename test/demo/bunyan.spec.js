c22fdea81eb988b648b7a0c5b365d86f42313e70
Pan Rufeng <xingzhikong@126.com>
2018-11-29 10:12:05 +0800

    Add files via upload

--- test/demo/bunyan.spec.js
+++ test/demo/bunyan.spec.js
@@ -1,7 +1,17 @@
 var expect = require('chai').expect;
 var bunyan = require('../../demo/bunyan');
 
-describe('Bunyan Logger test', function() {
-    bunyan.test();
-    // expect(true).to.be.true;
+describe('Bunyan Logger test', function () {
+    it('should output JSON log in terminal', function () {
+        var log = require('bunyan').createLogger({ name: 'app' });
+        log.info('hello world');
+        log.warn({ key: 'test' }, 'hello world');
+        log.error('hello world');
+    });
+    it('should output JSON log in file', function(){
+        const log = bunyan.log;
+        log.info('hello world');
+        log.warn({ key: 'test' }, 'hello world');
+        log.error('hello world');
+    });
 });
