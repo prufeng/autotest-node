bd172cb1395eeb68c0542d1c759ecd0791823cbc
Pan Rufeng <xingzhikong@126.com>
2018-11-29 10:11:29 +0800

    Add files via upload

--- demo/bunyan.js
+++ demo/bunyan.js
@@ -1,47 +1,37 @@
 var bunyan = require('bunyan');
-var log = bunyan.createLogger({ name: 'app' });
-log.info('hello world');
-log.warn({ key: 'test' }, 'hello world');
-log.error('hello world');
 
-var log2file = bunyan.createLogger({
-    name: 'log2file',
-    streams: [{
-        // type: 'file',  //default type
-        path: './logs/bunyan.log',
-    },
-    {
-        type: 'rotating-file',
-        path: './logs/bunyan-rf.log',
-        period: '1d',
-        count: 3
-    },
-    {
-        type: 'rotating-file',
-        level: 'error',
-        path: './logs/bunyan-rf-error.log',
-        period: '1d',
-        count: 3
-    }]
-});
-
-function test() {
-    log2file.info('hello world');
-    log2file.warn({ key: 'test' }, 'hello world');
-    log2file.error('hello world');
-}
-
-var express = {
-    name: 'bunyan-express',
-    // format: ":remote-address - :user-agent[major] custom logger",
+var log = bunyan.createLogger({
+    name: 'bunyan',
     streams: [
         {
             type: 'rotating-file',
-            path: './logs/bunyan-rf-express.log',
+            path: './logs/bunyan-app.log',
+            period: '1d',
+            count: 3
+        },
+        {
+            type: 'rotating-file',
+            level: 'error',
+            path: './logs/bunyan-error.log',
+            period: '1d',
+            count: 3
+        }]
+});
+
+var expressConfig = {
+    name: 'bunyan-express',
+    streams: [
+        {
+            type: 'rotating-file',
+            path: './logs/bunyan-express.log',
             period: '1d',
             count: 3
         }
     ]
 };
 
-module.exports = { test, express };
+function connectLogger(app){
+    app.use(require('express-bunyan-logger')(expressConfig));
+}
+
+module.exports = { log, connectLogger};
