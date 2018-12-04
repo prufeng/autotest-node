7df1d539c153383c19487a8adcd9d31e4bfc3791
Pan Rufeng <xingzhikong@126.com>
2018-12-04 17:54:58 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var log = require('../demo/log4js').getLogger('users', router);
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  log.info('respond with a resource');
   res.send('respond with a resource');
 });
 
