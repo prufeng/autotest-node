e759b3dc7b27cef530cd94c586558cedd8a6eb01
Pan Rufeng <xingzhikong@126.com>
2018-12-04 17:54:28 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var log = require('../demo/log4js').getLogger('index', router);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  log.info('index');
   res.render('index', { title: 'Express' });
 });
 
