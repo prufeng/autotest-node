9729d82f64d63caf589fcc98f5b4c5166e89ffaf
Pan Rufeng <xingzhikong@126.com>
2018-12-04 17:53:31 +0800

    Set Param for Log4js addContext Test

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.all('*', require('./demo/SetParamRouter'));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
